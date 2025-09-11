cff03655b46659cc9c9eb0fe8b82e6811ee17afd
Jyoti Kumari <btera7330@gmail.com>
2025-09-11 12:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (34.15%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -8,9 +8,25 @@ class Solution:
     def __init__(self):
         self.res=[]
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root:
-            self.postorderTraversal(root.left)
-            self.postorderTraversal(root.right)
-            self.res.append(root.val)
+        # if root:
+        #     self.postorderTraversal(root.left)
+        #     self.postorderTraversal(root.right)
+        #     self.res.append(root.val)
 
+        # return self.res
+
+        # using 2 stacks
+        if root is None:
+            return []
+        first_st=[root]
+        second_st=[]
+        while len(first_st)>0:
+            node=first_st.pop()
+            second_st.append(node)
+            if node.left :
+                first_st.append(node.left)
+            if node.right:
+                first_st.append(node.right)
+        for i in range(len(second_st)):
+            self.res.append(second_st.pop().val)
         return self.res
