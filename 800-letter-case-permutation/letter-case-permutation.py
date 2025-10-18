4515b711a2a8975e481ef115845710ca40ddf3dc
Jyoti Kumari <btera7330@gmail.com>
2025-10-18 15:28:41 +0530

    Time: 131 ms (5.43%) | Memory: 18.6 MB (72.94%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -5,6 +5,25 @@ class Solution:
         ans=[]
         self.solve(ip,op,ans)
         return ans
+    # def solve(self,ip,op,ans):
+    #     if len(ip)==0:
+    #         if op not in ans:
+    #             ans.append(op)
+    #         return
+    #     op1=op
+    #     op2=op
+    #     if ip[0].isdigit():
+    #         op1+=ip[0]
+    #         op2+=ip[0]
+    #     else:
+    #         op1+=ip[0].lower()
+    #         op2+=ip[0].upper()
+    #     ip=ip[1:]
+    #     self.solve(ip,op1,ans)
+    #     self.solve(ip,op2,ans)
+    # the above one is also correct but it has extra calls increasing repetetions and not very efficient
+    
+
     def solve(self,ip,op,ans):
         if len(ip)==0:
             if op not in ans:
@@ -14,12 +33,16 @@ class Solution:
         op2=op
         if ip[0].isdigit():
             op1+=ip[0]
-            op2+=ip[0]
+            ip=ip[1:]
+            self.solve(ip,op1,ans)
+            #when digit only one call
         else:
             op1+=ip[0].lower()
             op2+=ip[0].upper()
-        ip=ip[1:]
-        self.solve(ip,op1,ans)
-        self.solve(ip,op2,ans)
+            ip=ip[1:]
+            #when alpha ,it will call for 2 ..one with lower and one with upper case
+            self.solve(ip,op1,ans)
+            self.solve(ip,op2,ans)
+        
         
         
