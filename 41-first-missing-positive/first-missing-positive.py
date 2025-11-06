8d87530aef86959b5a1ead63eb9b7029a9b237ef
Jyoti Kumari <btera7330@gmail.com>
2025-11-06 13:35:46 +0530

    Time: 24 ms (84.45%) | Memory: 34.8 MB (6.41%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,24 +1,31 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        if len(nums)==0:
-            return 1
+    #     if len(nums)==0:
+    #         return 1
         
         
-        i=0
-        while i<len(nums):
-            idx=i+1
-            if nums[i]<len(nums) and nums[i]>0 and nums[i] !=nums[nums[i]-1]:  
-                    self.swap(i,nums[i]-1,nums)       
-            else:
-                i+=1
+    #     i=0
+    #     while i<len(nums):
+    #         idx=i+1
+    #         if nums[i]<len(nums) and nums[i]>0 and nums[i] !=nums[nums[i]-1]:  
+    #                 self.swap(i,nums[i]-1,nums)       
+    #         else:
+    #             i+=1
         
-        for i in range(len(nums)):
-            if ((i+1)!=nums[i]):
-                return i+1
-        if not nums:
-            return 1
-        return nums[-1]+1
+    #     for i in range(len(nums)):
+    #         if ((i+1)!=nums[i]):
+    #             return i+1
+    #     if not nums:
+    #         return 1
+    #     return nums[-1]+1
 
-    def swap(self,a,b,arr):
-        arr[a],arr[b]=arr[b],arr[a]
-        return arr
+    # def swap(self,a,b,arr):
+    #     arr[a],arr[b]=arr[b],arr[a]
+    #     return arr
+        s=set(nums)
+        n=len(nums)
+        ans=n+1
+        for i in range(1,n+1):
+            if i not in s:
+                return i
+        return ans
