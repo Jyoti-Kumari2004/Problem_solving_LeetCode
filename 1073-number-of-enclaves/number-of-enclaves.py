6a8ef36cb921aaf3a9bd6a96a6942b31ac4b37d0
Jyoti Kumari <btera7330@gmail.com>
2025-12-01 16:14:17 +0530

    Time: 163 ms (18.12%) | Memory: 57.7 MB (5.74%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numEnclaves(self, board: List[List[int]]) -> int:
+        n=len(board)
+        m=len(board[0])
+        count=0
+        visited_mat=[[0 for i in range(m)]for i in range(n)]
+        for i in range(m):
+            if board[0][i]==1 and visited_mat[0][i]==0:
+                self.dfs(0,i,board,visited_mat)
+        for i in range(n):
+            if board[i][0]==1 and visited_mat[i][0]==0:
+                self.dfs(i,0,board,visited_mat)
+        for i in range(m):
+            if board[n-1][i]==1 and visited_mat[n-1][i]==0:
+                self.dfs(n-1,i,board,visited_mat)
+        for i in range(n):
+            if board[i][m-1]==1 and visited_mat[i][m-1]==0:
+                self.dfs(i,m-1,board,visited_mat)
+        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == 1 and visited_mat[i][j] == 0:
+                    count+=1
+        return count
+
+
+        
+    def dfs(self,x,y,board,visited_mat):
+        dir=[[0,-1],[0,1],[1,0],[-1,0]]
+        visited_mat[x][y]=1
+        for i,j in dir:
+            if 0<=x+i<len(board) and 0<=y+j<len(board[0]) and board[x+i][y+j]==1 and visited_mat[x+i][y+j]==0:
+                self.dfs(x+i,y+j,board,visited_mat)
+
+        
