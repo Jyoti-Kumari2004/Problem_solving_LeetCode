d206fe0c4fff193f5c23c8f30c05ed91e24b0f0a
Jyoti Kumari <btera7330@gmail.com>
2025-12-20 12:31:54 +0530

    Time: 11 ms (5.14%) | Memory: 19.6 MB (99.09%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.anss=[]
+        self.w=0
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root==None:
+            return None
+        ans=[]
+        self.inorder(root,ans)
+        print(ans)
+        check=[0]*len(ans)
+        check[0]=sum(ans)
+        for i in range(1,len(ans)):
+            check[i]=check[i-1]-ans[i-1]
+        print(check)
+        w=0
+        self.inorderA(root,check,w)
+        print(self.anss)
+        return root
+        
+        
+    def inorderA(self,root,check,w):
+        if root!=None:
+            self.inorderA(root.left,check,self.w)
+            root.val=check[self.w]
+            self.anss.append(root.val)
+            self.w+=1
+            self.inorderA(root.right,check,self.w)
+    
+    def inorder(self,root,ans):
+        if root!=None:
+            self.inorder(root.left,ans)
+            ans.append(root.val)
+            self.inorder(root.right,ans)
+        
+    
+        
