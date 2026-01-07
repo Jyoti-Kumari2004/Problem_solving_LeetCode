8690a9041a7f0c0c0e106cf20a36ea6c1fd1f57c
Jyoti Kumari <btera7330@gmail.com>
2026-01-07 10:00:36 +0530

    Time: 762 ms (25.90%) | Memory: 27.6 MB (82.56%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -0,0 +1,32 @@
+class Solution:
+    def __init__(self):
+        self.t=[[-1]*1001 for i in range(1002)]
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n=len(text1)
+        m=len(text2)
+        return self.LCS(text1,text2,n,m)
+
+    def LCS(self,X,Y,n,m):
+        if n==0 or m==0:
+            return 0
+        if self.t[n][m]!=-1:
+            return self.t[n][m]
+        elif X[n-1]==Y[m-1]:
+            self.t[n][m]=1+self.LCS(X,Y,n-1,m-1)
+        else:
+            self.t[n][m]= max(self.LCS(X,Y,n-1,m),self.LCS(X,Y,n,m-1))
+
+        return self.t[n][m]
+
+#recursive approach:
+    # class Solution:
+    #     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    #         return self.LCS(text1,text2,len(text1),len(text2))
+
+    #     def LCS(self,X,Y,n,m):
+    #         if n==0 or m==0:
+    #             return 0
+    #         elif X[n-1]==Y[m-1]:
+    #             return 1+self.LCS(X,Y,n-1,m-1)
+    #         else:
+    #             return max(self.LCS(X,Y,n-1,m),self.LCS(X,Y,n,m-1))
