8f701c846b8665e9efe8de41c3d6bfcc89c752c4
Jyoti Kumari <btera7330@gmail.com>
2026-01-21 17:34:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.80%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,19 +2,30 @@ class Solution:
     def __init__(self):
         self.t=[-1 for i in range(101)]
     def rob(self, nums: List[int]) -> int:
-        return self.solve(nums,len(nums)-1)
-        
-    def solve(self,nums,i):
-        if i==0 :
-            return nums[i]
-        if i<0:
-            return 0
-        if self.t[i]!=-1:
-            return self.t[i]
-        ch=nums[i]+self.solve(nums,i-2)
-        ch2=self.solve(nums,i-1)
-        self.t[i]=max(ch,ch2)
-        return self.t[i]
+        return self.solve(nums)
+    def solve(self,nums):
+        t=[0]*len(nums)
+        t[0]=nums[0]
+        for i in range(1,len(nums)):
+            ch=nums[i]+(t[i-2] if i>1  else 0)
+            ch2=t[i-1]
+            t[i]=max(ch,ch2)
+        return t[len(nums)-1]
+
+    #memoiation solution: 
+    # def solve(self,nums,i):
+    #     if i==0 :
+    #         return nums[i]
+    #     if i<0:
+    #         return 0
+    #     if self.t[i]!=-1:
+    #         return self.t[i]
+    #     ch=nums[i]+self.solve(nums,i-2)
+    #     ch2=self.solve(nums,i-1)
+    #     self.t[i]=max(ch,ch2)
+    #     return self.t[i]
+
+    #recurrsive solution:
     # def solve(self,nums,i):
     #     if i==0 :
     #         return nums[i]
