04dcf4c2fafa29299fb7111687dcd17f79844457
Jyoti Kumari <btera7330@gmail.com>
2026-02-17 17:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (46.53%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,9 +1,12 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res=[]
-        temp=[]
-        self.helper(res,temp,nums)
-        return res
+        # res=[]
+        # temp=[]
+        # self.helper(res,temp,nums)
+        # return res
+        result=[]
+        self.Permute(nums,[],result)
+        return result
 
     def helper(self,res,temp,nums):
         if len(temp)==len(nums):
@@ -15,5 +18,17 @@ class Solution:
             temp.append(num)
             self.helper(res,temp,nums)
             temp.pop()
+    def Permute(self,ip,op,result):
+        if len(ip)==0:
+            result.append(op.copy())
+            return 
+        s=set()
+        for i in range(len(ip)):
+            if ip[i] not in s:
+                new_ip=ip[:i]+ip[i+1:]
+                op.append(ip[i])
+                self.Permute(new_ip,op,result)
+                op.pop()
+        
 
         
