ab575fcee49d477755f6fd1d24348cca141ea5db
Jyoti Kumari <btera7330@gmail.com>
2026-02-23 18:48:39 +0530

    Time: 7 ms (96.55%) | Memory: 19.7 MB (55.76%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,7 +1,14 @@
 class Solution:
+    def __init__(self):
+        self.row_side=[]
+        self.up_dg=[]
+        self.down_dg=[]
     def solveNQueens(self, n: int) -> List[List[str]]:
         board=[["."]*n for i in range(n)]
         res=[]
+        self.row_side=[False]*n
+        self.up_dg=[False]*((2*n)-1)
+        self.down_dg=[False]*((2*n)-1)
         self.solve(n,res,0,board)
         return res
     def solve(self,n,res,col,board):
@@ -13,33 +20,45 @@ class Solution:
             res.append(path.copy())
             return 
         for i in range(0,n):
-            if self.isValid(n,i,col,board):
-                board[i][col]="Q"
+            
+            if self.row_side[i]==False and self.up_dg[(n-1)+(col-i)]==False and self.down_dg[i+col]==False: 
+             
+                board[i][col]="Q" 
+
+                self.row_side[i]=True
+                self.up_dg[(n-1)+(col-i)]=True
+                self.down_dg[i+col]=True
+
                 self.solve(n,res,col+1,board)
+
                 board[i][col]="."
-    def isValid(self,n,row,col,board):
-        ori_row=row
-        ori_col=col
+
+                self.row_side[i]=False
+                self.up_dg[(n-1)+(col-i)]=False
+                self.down_dg[i+col]=False
+    # def isValid(self,n,row,col,board):
+    #     ori_row=row
+    #     ori_col=col
         
-        while 0<=row<n and 0<=col<n:
-            if board[row][col]=="Q":
-                return False
-            row-=1
-            col-=1
-        row=ori_row
-        col=ori_col
-        while 0<=row<n and 0<=col<n:
-            if board[row][col]=="Q":
-                return False
-            col-=1
-        row=ori_row
-        col=ori_col
-        while 0<=row<n and 0<=col<n:
-            if board[row][col]=="Q":
-                return False
-            col-=1
-            row+=1
-        return True
+    #     while 0<=row<n and 0<=col<n:
+    #         if board[row][col]=="Q":
+    #             return False
+    #         row-=1
+    #         col-=1
+    #     row=ori_row
+    #     col=ori_col
+    #     while 0<=row<n and 0<=col<n:
+    #         if board[row][col]=="Q":
+    #             return False
+    #         col-=1
+    #     row=ori_row
+    #     col=ori_col
+    #     while 0<=row<n and 0<=col<n:
+    #         if board[row][col]=="Q":
+    #             return False
+    #         col-=1
+    #         row+=1
+    #     return True
 
 
 
