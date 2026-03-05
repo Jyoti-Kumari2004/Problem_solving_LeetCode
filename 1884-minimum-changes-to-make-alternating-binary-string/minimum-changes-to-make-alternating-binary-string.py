1cbf98c9398495a022a5aac469ff7e18cfc46195
Jyoti Kumari <btera7330@gmail.com>
2026-03-05 10:40:14 +0530

    Time: 7 ms (83.47%) | Memory: 19.3 MB (56.18%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -6,11 +6,11 @@ class Solution:
                 count1+=1
             elif i%2==1 and s[i]!="1":
                 count1+=1
-        count2=0
-        for i in range(len(s)):
-            if i%2==0 and s[i]!="1":
-                count2+=1
-            elif i%2==1 and s[i]!="0":
-                count2+=1
-        return min(count1,count2)
+        # count2=0
+        # for i in range(len(s)):
+        #     if i%2==0 and s[i]!="1":
+        #         count2+=1
+        #     elif i%2==1 and s[i]!="0":
+        #         count2+=1
+        return min(count1,len(s)-count1)
         
