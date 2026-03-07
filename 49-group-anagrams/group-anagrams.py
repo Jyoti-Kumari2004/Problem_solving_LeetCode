11928fde1aa62e18ca101621917d8b67f7cdcf65
Jyoti Kumari <btera7330@gmail.com>
2026-03-07 20:17:59 +0530

    Time: 11 ms (84.75%) | Memory: 22.9 MB (38.77%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,24 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        li=[]
-        for i in range(len(strs)):
-            d=defaultdict(int)
-            for j in strs[i]:
-                d[j]+=1
-            sorted_dict = dict(sorted(d.items()))
-            li.append(sorted_dict)
         res=[]
-        visited=[False]*len(strs)
-        print(li)
-        for j in range(len(li)):
-            ans=[]
-            for k in range(len(li)):
-                if li[j]==li[k] and visited[k]==False:
-                    ans.append(strs[k])
-                    visited[k]=True
-            if ans:
-                ans.sort()
-                res.append(ans)
-        res.sort()
-        return res
-            
+        for word in strs:
+            res.append([word,"".join(sorted(word))])
+        d=defaultdict(list)
+        for word,st in res:
+            d[st].append(word)
+        ans=[]
+        for lis in d.values():
+            ans.append(lis)
+        return ans
+        
