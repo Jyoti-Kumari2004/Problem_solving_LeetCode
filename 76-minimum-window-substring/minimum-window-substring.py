87a49a24c6c87c24b6475d246c89601e3b4155c2
Jyoti Kumari <btera7330@gmail.com>
2026-03-14 16:17:58 +0530

    Time: 61 ms (78.55%) | Memory: 19.6 MB (90.88%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,30 +1,34 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        i,j,count=0,0,0
-        m=float('inf')
-        res=""
-        map=defaultdict(int)
+        if len(t)>len(s):
+            return ""
+        d=defaultdict(int)
         for ch in t:
-            map[ch]+=1
-        count=len(map)
-        print(map)
+            d[ch]+=1
+        count=len(d)
+        bi=0
+        bj=0
+        i=0
+        j=0
+        ans=float('inf')
         while j<len(s):
-            if s[j] in map:
-                map[s[j]]-=1
-                if map[s[j]]==0:
-                    count-=1   
-            #slid and check upto that number
+            if s[j] in d:
+                d[s[j]]-=1
+                if d[s[j]]==0:
+                    count-=1
             while count==0:
-                if j-i+1<m:
-                    m=(j-i+1)
-                    res=s[i:i+m]
-                if s[i] in map:
-                    map[s[i]]+=1
-                    if map[s[i]]==1:
+                if j-i+1<ans:
+                    bi=i
+                    bj=j
+                    ans=j-i+1
+                if s[i] in d:
+                    d[s[i]]+=1
+                    if d[s[i]]==1:
                         count+=1
                 i+=1
             j+=1
-        return res
-
+        if ans==float('inf'):
+            return ""
+        return s[bi:bj+1]
 
         
