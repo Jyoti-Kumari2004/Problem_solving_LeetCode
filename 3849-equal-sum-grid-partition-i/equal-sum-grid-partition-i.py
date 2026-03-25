ee6b9c00db9e806b6c3818b5f5d9d3a20577815f
Jyoti Kumari <btera7330@gmail.com>
2026-03-25 22:51:35 +0530

    Time: 221 ms (22.60%) | Memory: 41.4 MB (86.30%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.py
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.py
@@ -5,24 +5,28 @@ class Solution:
         s=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                curr+=grid[i][j]
                 s+=grid[i][j]
-            hori[i]=curr
-        veri=[0]*len(grid[0])
+        curr=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                curr+=grid[i][j]
+            if curr==s-curr:
+                    return True 
         curr=0
         for j in range(len(grid[0])):
             for i in range(len(grid)):
                 curr+=grid[i][j]
-            veri[j]=curr
-        
-        hs=s
-        vs=s
-        for i in range(len(hori)):
-            if hori[i]==hs-hori[i]:
+            if curr==s-curr:
                 return True
-        for j in range(len(veri)):
-            if veri[j]==vs-veri[j]:
-                return True
-        
         return False
+        # hs=s
+        # vs=s
+        # for i in range(len(hori)):
+        #     if hori[i]==hs-hori[i]:
+        #         return True
+        # for j in range(len(veri)):
+        #     if veri[j]==vs-veri[j]:
+        #         return True
+        
+        # return False
 
