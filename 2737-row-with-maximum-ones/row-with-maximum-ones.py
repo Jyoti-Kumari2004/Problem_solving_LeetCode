308adad4b52fa9f1ee77bf14a0d33ce36c4c1856
Jyoti Kumari <btera7330@gmail.com>
2026-03-26 16:19:29 +0530

    Time: 31 ms (8.74%) | Memory: 20 MB (87.15%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.py
@@ -1,19 +1,24 @@
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        ans=[0]*len(mat)
+        ans=0
+        bi=0
+        maxi=0
         for i in range(len(mat)):
             curr=0
             for j in range(len(mat[0])):
                 if mat[i][j]==1:
                     curr+=1
-            ans[i]=curr
-        res=0
-        maxi=ans[0]
-        for i in range(len(ans)):
-            if ans[i]>maxi:
-                maxi=ans[i]
-                res=i
-        return [res,maxi]
+            if maxi<curr:
+                maxi=curr
+                bi=i
+        return [bi,maxi]
+        # res=0
+        # maxi=ans[0]
+        # for i in range(len(ans)):
+        #     if ans[i]>maxi:
+        #         maxi=ans[i]
+        #         res=i
+        # return [res,maxi]
 
 
     
