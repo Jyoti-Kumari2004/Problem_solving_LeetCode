502ec51e8c65af68666519c21e7407b195911d43
Jyoti Kumari <btera7330@gmail.com>
2026-03-31 00:45:12 +0530

    Time: 195 ms (23.13%) | Memory: 19.8 MB (100.00%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,22 +1,20 @@
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        s1o=[]
-        s1e=[]
-        s2o=[]
-        s2e=[]
+        odalpha=[0]*26
+        evalpha=[0]*26
+
         for i in range(len(s1)):
             if i%2==0:
-                s1e.append(s1[i])
+                odalpha[ord('a')-ord(s1[i])]+=1
             else:
-                s1o.append(s1[i])
+                evalpha[ord('a')-ord(s1[i])]+=1
         for j in range(len(s2)):
             if j%2==0:
-                s2e.append(s2[j])
+                odalpha[ord('a')-ord(s2[j])]-=1
             else:
-                s2o.append(s2[j])
-        # s1e.sort()
-        # s1o.sort()
-        # s2e.sort()
-        # s2o.sort()
-        # print(s1e,s2e,s1o,s2o)
-        return sorted(s1e)==sorted(s2e) and sorted(s1o)==sorted(s2o)
+                evalpha[ord('a')-ord(s2[j])]-=1
+        for i in range(len(odalpha)):
+            if odalpha[i]!=0 or evalpha[i]!=0:
+                return False
+        return True
+        
