203a58821671fa86af2af005dfa4e54b763f3286
Jyoti Kumari <btera7330@gmail.com>
2026-04-01 17:44:41 +0530

    Time: 63 ms (18.76%) | Memory: 24.7 MB (33.35%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,69 +1,50 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        l1=matrix[0]
-        prev=list(map(int, l1))
-        
-        ans=self.getMaxArea(list(map(int, l1)))
-        for i in range(1,len(matrix)):
-            li=[]
+        li=[0]*len(matrix[0])
+        ans=0
+        for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j]=="1":
-                    li.append(int(matrix[i][j])+prev[j])
+                    li[j]+=1
                 else:
-                    li.append(0)
-                temp=self.getMaxArea(list(map(int, li)))
-                ans=max(ans,temp)
-            prev=li
+                    li[j]=0
+            print(li)
+            ans=max(self.LargestRectangleinHistogram(li),ans)
         return ans
-        
+    def LargestRectangleinHistogram(self,li):
+        x=self.nextSmaller(li)
+        y=self.prevSmaller(li)
+        max_area=0
+        for i in range(len(li)):
+            height=li[i]
+            width=x[i]-y[i]-1
+            max_area=max(max_area,height*width)
+        return max_area
 
 
-        
-    def getMaxArea(self, arr):
-        # code here
-        ns=self.nextSmaller(arr)
-        ps=self.prevSmaller(arr)
-        area=0
-        
-        for i in range(len(arr)):
-            height=arr[i]
-            width=ns[i]-ps[i]-1
-            area=max(area,height*width)
-        return area
-            
-    def nextSmaller(self,arr):
+
+    def prevSmaller(self,li):
         st=[]
-        res=[]
-        for i  in range(len(arr)-1,-1,-1):
+        ans=[]
+        for i in range(len(li)):
+            while st and li[i]<=li[st[-1]]:
+                st.pop()
             if not st:
-                res.append(len(arr))
-            elif arr[st[-1]]<arr[i]:
-                res.append(st[-1])
+                ans.append(-1)
             else:
-                while st and arr[st[-1]]>=arr[i]:
-                    st.pop()
-                if not st:
-                    res.append(len(arr))
-                else:
-                    res.append(st[-1])
+                ans.append(st[-1])
             st.append(i)
-        return res[::-1]
-    def prevSmaller(self,arr):
+        return ans
+    
+    def nextSmaller(self,li):
         st=[]
-        res=[]
-        for i  in range(len(arr)):
+        ans=[]
+        for i in range(len(li)-1,-1,-1):
+            while st and li[i]<=li[st[-1]]:
+                st.pop()
             if not st:
-                res.append(-1)
-            elif arr[st[-1]]<arr[i]:
-                res.append(st[-1])
+                ans.append(len(li))
             else:
-                while st and arr[st[-1]]>=arr[i]:
-                    st.pop()
-                if not st:
-                    res.append(-1)
-                else:
-                    res.append(st[-1])
+                ans.append(st[-1])
             st.append(i)
-        return res
-        
-        
+        return ans[::-1]
