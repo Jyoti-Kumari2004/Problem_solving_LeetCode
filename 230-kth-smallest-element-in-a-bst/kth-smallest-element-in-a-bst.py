fc5e43f6ba705d28d770f9ab3fce749c4a0996f5
Jyoti Kumari <btera7330@gmail.com>
2026-04-07 16:14:32 +0530

    Time: 1 ms (39.42%) | Memory: 22.4 MB (29.36%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,16 +6,39 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        q=[]
-        self.solve(root,q,k)
-        return -q[0]
-    def solve(self,root,q,k):
-        if root!=None:
-            heapq.heappush(q,-root.val) 
-            if len(q)>k:
-                heapq.heappop(q)
-            self.solve(root.right,q,k)
-            self.solve(root.left,q,k)
+        # q=[]
+        # self.solve(root,q,k)
+        # return -q[0]
+
+        #second approach
+        res=[]
+        self.solveEffi(root,res,k)
+        return res[-1]
+
+    def solveEffi(self,root,res,k):
+        if root==None:
+            return False
+        ls=self.solveEffi(root.left,res,k)
+        if ls :return True
+        res.append(root.val)
+        print(res)
+        if len(res)==k:
+            return True
+        rs=self.solveEffi(root.right,res,k)
+        if rs:return True
+        
+
+
+
+
+    #since this method is inefficient , as this take O(no.of nodes),but we can solve this in O(n) property
+    # def solve(self,root,q,k):
+    #     if root!=None:
+    #         heapq.heappush(q,-root.val) 
+    #         if len(q)>k:
+    #             heapq.heappop(q)
+    #         self.solve(root.right,q,k)
+    #         self.solve(root.left,q,k)
     
 
         
