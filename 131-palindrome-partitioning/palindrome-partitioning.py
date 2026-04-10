71bceb87c8ffc43f8cd0371a2c9a04a1be2a2107
Jyoti Kumari <btera7330@gmail.com>
2026-04-10 15:51:26 +0530

    Time: 31 ms (93.40%) | Memory: 33.4 MB (93.23%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,25 +1,37 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res=[]
-        self.solve(s,0,res,[])
-        return res
+        n=len(s)
+        m=len(s)
+        pt=[[False]*m for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if i==j:
+                    pt[i][j]=True
+        for i in range(n-1,-1,-1):
+            for j in range(m):
+                if j>i:
+                    l=j-i+1
+                    if s[i]==s[j]:
+                        if l==2 or l==3:
+                            pt[i][j]=True
+                        else:
+                            if pt[i+1][j-1]:
+                                pt[i][j]=True
+                            else:
+                                pt[i][j]=False
+                    else:
+                        pt[i][j]=False
 
-    def solve(self,s,ind,res,path):
+        self.solve(0,s,pt,res,[])
+        return res
+    def solve(self,ind,s,pt,res,path):
         if ind==len(s):
             res.append(path.copy())
             return 
         for i in range(ind,len(s)):
-            if self.isPanil(s[ind:i+1])==True:
-                path.append(s[ind:i+1])
-                self.solve(s,i+1,res,path)
+            if pt[ind][i]:
+                path.append(s[ind:i+1]) 
+                self.solve(i+1,s,pt,res,path)
                 path.pop()
         
-    def isPanil(self,s):
-        i=0
-        j=len(s)-1
-        while i<j:
-            if s[i]!=s[j]:
-                return False
-            i+=1
-            j-=1
-        return True
