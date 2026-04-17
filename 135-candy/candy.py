1546f0b022fc0f480897111a41ab5a19f43e989b
Jyoti Kumari <btera7330@gmail.com>
2026-04-17 19:17:44 +0530

    Time: 7 ms (98.95%) | Memory: 21.5 MB (53.92%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,24 +1,71 @@
 class Solution:
+    #solving this using greedy, in O(n) time and O(1) space complexity
     def candy(self, ratings: List[int]) -> int:
-        pref=[0]*len(ratings)
-        suff=[0]*len(ratings)
-        pref[0]=1
-        for i in range(1,len(ratings)):
-            if ratings[i]>ratings[i-1]:
-                pref[i]=pref[i-1]+1
-            else:
-                pref[i]=1
-        suff=1
-        summ=max(1,pref[-1])
-        for i in range(len(ratings)-2,-1,-1):
-            if ratings[i]>ratings[i+1]:
-                suff=suff+1
-            else:
-                suff=1
-            summ+=max(pref[i],suff)
-        print(suff)
-        return summ
+        s,i=1,1
+        while i<len(ratings):
+            if ratings[i]==ratings[i-1]:
+                s+=1
+                i+=1
+                continue
+            peak=1
+            while i<len(ratings) and ratings[i]>ratings[i-1]:
+                peak+=1
+                s+=peak
+                i+=1
+            down=1
+            while i<len(ratings) and ratings[i]<ratings[i-1]:
+                s+=down
+                down+=1
+                i+=1
+            if down>peak:
+                s+=down-peak
+        return s
+            
+
+
+
+
+
+
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #this is also takein O(n) time and O(n) space, i need to remove the space
+    # def candy(self, ratings: List[int]) -> int:
+    #     pref=[0]*len(ratings)
+    #     suff=[0]*len(ratings)
+    #     pref[0]=1
+    #     for i in range(1,len(ratings)):
+    #         if ratings[i]>ratings[i-1]:
+    #             pref[i]=pref[i-1]+1
+    #         else:
+    #             pref[i]=1
+    #     suff=1
+    #     summ=max(1,pref[-1])
+    #     for i in range(len(ratings)-2,-1,-1):
+    #         if ratings[i]>ratings[i+1]:
+    #             suff=suff+1
+    #         else:
+    #             suff=1
+    #         summ+=max(pref[i],suff)
+    #     print(suff)
+    #     return summ
+        
+
 
 
     #using first naive approach , this is O(4n) time and O(3n) space
@@ -43,4 +90,5 @@ class Solution:
     #     for m,n in zip(pref,suff):
     #         res.append(max(m,n))
     #     return sum(res)
+        
         
