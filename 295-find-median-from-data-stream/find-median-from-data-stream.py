2f92b9eebb556494765fa65cf89c5b72e864dda3
Jyoti Kumari <btera7330@gmail.com>
2026-05-25 14:42:35 +0530

    Time: 191 ms (49.00%) | Memory: 41.7 MB (81.79%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,22 +1,42 @@
 class MedianFinder:
 
     def __init__(self):
-        self.nums=[]
+        self.l=[]
+        self.r=[]
         
 
     def addNum(self, num: int) -> None:
-        self.nums.append(num)
+        heapq.heappush(self.l,-num)
+        if self.r and self.l and -(self.l[0])>self.r[0]:
+            ele=heapq.heappop(self.l)
+            heapq.heappush(self.r,-ele)
+
+
+        if len(self.l)-len(self.r)>1:
+            ele=heapq.heappop(self.l)
+            heapq.heappush(self.r,-ele)
+        if len(self.r)-len(self.l)>1:
+            ele=heapq.heappop(self.r)
+            heapq.heappush(self.l,-ele)
+
         
 
     def findMedian(self) -> float:
-        self.nums.sort()
-        n=len(self.nums)
-        if n%2!=0:
-            return self.nums[n//2]
+        if len(self.l)==len(self.r):
+            ele1=heapq.heappop(self.l)
+            ele2=heapq.heappop(self.r)
+            heapq.heappush(self.l,ele1)
+            heapq.heappush(self.r,ele2)
+            return ((-ele1)+ele2)/2
+        elif len(self.l)>len(self.r):
+            ele1=heapq.heappop(self.l)
+            heapq.heappush(self.l,ele1)
+            return -ele1
         else:
-            m1=self.nums[n//2]
-            m2=self.nums[(n//2)-1]
-            return (m1+m2)/2
+            ele2=heapq.heappop(self.r)
+            heapq.heappush(self.r,ele2)
+            return ele2
+
 
         
 
