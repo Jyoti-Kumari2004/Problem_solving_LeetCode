a938580044a95e2e9b5e2f1a309d46a9ccfaccce
Jyoti Kumari <btera7330@gmail.com>
2026-06-03 22:49:32 +0530

    Time: 52 ms (26.96%) | Memory: 20.1 MB (19.63%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,30 +9,70 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        d=defaultdict(int)
+        if not head:
+            return None
         temp=head
         while temp!=None:
             new_node=Node(temp.val)
-            d[temp]=new_node
-            temp=temp.next
-        print(d)
+            next=temp.next
+            temp.next=new_node
+            new_node.next=next
+            temp=temp.next.next
         temp=head
-        tail=Node(-1)
-        h=tail
-        while temp!=None:
-            node=d[temp]
-            if temp.next!=None:
-                node.next=d[temp.next]
+        while temp:
+            if temp.random:
+                temp.next.random=temp.random.next
             else:
-                node.next=None
-            if temp.random!=None:
-                node.random=d[temp.random]
-            else:
-                node.random=None
-            tail.next=node
-            tail=tail.next
-            temp=temp.next
-        tail.next=None
-        return h.next
+                temp.next.random=None
+            temp=temp.next.next
+        curr=head.next
+        nh=curr
+        while curr.next!=None:
+            curr.next=curr.next.next
+            curr=curr.next
+        return nh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # this is taking more time , not very efficient because this uses hashmap
+        # d=defaultdict(int)
+        # temp=head
+        # while temp!=None:
+        #     new_node=Node(temp.val)
+        #     d[temp]=new_node
+        #     temp=temp.next
+        # print(d)
+        # temp=head
+        # tail=Node(-1)
+        # h=tail
+        # while temp!=None:
+        #     node=d[temp]
+        #     if temp.next!=None:
+        #         node.next=d[temp.next]
+        #     else:
+        #         node.next=None
+        #     if temp.random!=None:
+        #         node.random=d[temp.random]
+        #     else:
+        #         node.random=None
+        #     tail.next=node
+        #     tail=tail.next
+        #     temp=temp.next
+        # tail.next=None
+        # return h.next
             
 
