684efe76f3a3de042d17356fc7b2154163720a30
Jyoti Kumari <btera7330@gmail.com>
2026-06-04 14:52:00 +0530

    Time: 2281 ms (5.13%) | Memory: 30.2 MB (44.38%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -2,32 +2,71 @@ class Solution:
     def __init__(self):
         self.t=[[[-1]*71 for i in range(71)] for j in range(71)]
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        return self.solve(0,0,len(grid[0])-1,grid)
+        t=[[[None]*(len(grid[0])) for i in range(len(grid[0]))] for j in range(len(grid))]
+        n=len(grid)
+        #making a possible base case
 
-
-    def solve(self,i,j1,j2,grid):
-        if i<0 or i>=len(grid) or j1<0 or j1>=len(grid[0]) or j2<0 or j2>=len(grid[0]):
-            return float("-inf")
-        if i==len(grid)-1:
-            if j1==j2:
-                return grid[i][j1]
-            else:
-                return grid[i][j1]+grid[i][j2]
-        # if self.t[i][j][k]!=-1:
-        #     return self.t[i][j][k]
-        if self.t[i][j1][j2]!=-1:
-            return self.t[i][j1][j2]
-        
-        ans=float("-inf")
-        for dj1 in range(-1,2):
-            for dj2 in range(-1,2):
-                if j1==j2:
-                    ch=grid[i][j1]+self.solve(i+1,j1+dj1,j2+dj2,grid)
+        for j1 in range(len(grid[0])):
+            for j2 in range(len(grid[0])):
+                if j1!=j2:
+                    t[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2]
                 else:
-                    ch=grid[i][j1]+grid[i][j2]+self.solve(i+1,j1+dj1,j2+dj2,grid) 
-                ans=max(ans,ch)
-        self.t[i][j1][j2]=ans
-        return ans
+                    t[n-1][j1][j2]=grid[n-1][j1]
+        for i in range(n-2,-1,-1):
+            for j1 in range(len(grid[0])):
+                for j2 in range(len(grid[0])):
+                    ans=float('-inf')
+                    for dj1 in range(-1,2):
+                        for dj2 in range(-1,2):
+                            if j1+dj1>=0 and j1+dj1<len(grid[0]) and j2+dj2>=0 and j2+dj2<len(grid[0]):
+                                if j1==j2:
+                                    ch=grid[i][j1]+t[i+1][j1+dj1][j2+dj2] 
+                                else:
+                                    
+                                        ch=grid[i][j1]+grid[i][j2]+t[i+1][j1+dj1][j2+dj2]
+                            else:
+                                ch=float('-inf')
+                            ans=max(ans,ch)
+                            
+                    t[i][j1][j2]=ans
+        return t[0][0][len(grid[0])-1]
+
+                    
+
+        
+
+
+
+
+
+
+
+
+
+# this is memoiation, still usese more space, need to optimize this more
+            # def solve(self,i,j1,j2,grid):
+            #     if i<0 or i>=len(grid) or j1<0 or j1>=len(grid[0]) or j2<0 or j2>=len(grid[0]):
+            #         return float("-inf")
+            #     if i==len(grid)-1:
+            #         if j1==j2:
+            #             return grid[i][j1]
+            #         else:
+            #             return grid[i][j1]+grid[i][j2]
+            #     # if self.t[i][j][k]!=-1:
+            #     #     return self.t[i][j][k]
+            #     if self.t[i][j1][j2]!=-1:
+            #         return self.t[i][j1][j2]
+                
+            #     ans=float("-inf")
+            #     for dj1 in range(-1,2):
+            #         for dj2 in range(-1,2):
+            #             if j1==j2:
+            #                 ch=grid[i][j1]+self.solve(i+1,j1+dj1,j2+dj2,grid)
+            #             else:
+            #                 ch=grid[i][j1]+grid[i][j2]+self.solve(i+1,j1+dj1,j2+dj2,grid) 
+            #             ans=max(ans,ch)
+            #     self.t[i][j1][j2]=ans
+            #     return ans
         # if i1==i2 and j1==j2:
         #     ch1=grid[i1][j1]+self.solve(i1+1,j1-1,i2+1,j2-1,grid)
         #     ch4=grid[i1][j1]+self.solve(i1+1,j1-1,i2+1,j2+1,grid)
