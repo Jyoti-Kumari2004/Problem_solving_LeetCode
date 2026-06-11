52a7def3c059d977f5c642114166cbab22830beb
Jyoti Kumari <btera7330@gmail.com>
2026-06-11 17:32:23 +0530

    Time: 361 ms (17.43%) | Memory: 19.6 MB (98.17%) - LeetSync

--- 2757-count-of-integers/count-of-integers.py
+++ 2757-count-of-integers/count-of-integers.py
@@ -1,13 +1,18 @@
 class Solution:
+    def __init__(self):
+        self.t=[[[-1]*401 for j in range(2)]for i in range(23)]
+
     def count(self, num1: str, num2: str, min_sum: int, max_sum: int) -> int:
+        self.t=[[[-1]*401 for j in range(2)]for i in range(23)]
         one=self.solve(0,1,num2,0,min_sum,max_sum)%1000000007
         n1=int(num1)
-    
+        self.t=[[[-1]*401 for j in range(2)]for i in range(23)]
         two=self.solve(0,1,str(n1-1),0,min_sum,max_sum)%1000000007
         print(one)
         print(two)
         return (one-two)%1000000007
-    @lru_cache(None)
+    #@lru_cache(None) # we can use this also, ..but i will also implement proper memoizaiton not the inbuilt things
+
     def solve(self,ind,tight,s,curr,mini,maxi):
         if curr>maxi:
             return 0
@@ -17,10 +22,13 @@ class Solution:
             newlimit=9
         else:
             newlimit=int(s[ind])
+        if self.t[ind][tight][curr]!=-1:
+            return self.t[ind][tight][curr]
         res=0
         for i in range(newlimit+1):
             newtight=1 if (i==int(s[ind]) and tight==1) else 0
             res+=self.solve(ind+1,newtight,s,curr+i,mini,maxi)
+        self.t[ind][tight][curr]=res
         return res
         
 
