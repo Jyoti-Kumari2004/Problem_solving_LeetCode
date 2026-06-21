d23ca69aba546f7b06855561d767970c209cf3a4
Jyoti Kumari <btera7330@gmail.com>
2026-06-21 11:19:00 +0530

    Time: 1375 ms (30.08%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
+-- select p.product_name,s.year,s.price
+-- from Product as p
+-- natural join Sales as s;
+
+-- or 
 select p.product_name,s.year,s.price
 from Product as p
-natural join Sales as s;
+inner join Sales as s
+where s.product_id=p.product_id;
