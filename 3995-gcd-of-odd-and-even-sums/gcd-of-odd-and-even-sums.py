4db0fc9955b9b32ef6de132479e5fd0a7d2e8fb6
Jyoti Kumari <btera7330@gmail.com>
2026-07-15 22:12:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (21.29%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -1,23 +1,24 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        ce=0
-        ev=0
-        od=0
-        ods=1
-        for i in range(n):
-            ev+=(ce+2)
-            ce=ce+2
-            od+=(ods+2)
-            ods=ods+2
-        print(ev,od)
-        return self.gcd(ev,od)   
-    def gcd(self,a,b):
-        if a == 0:
-            return b
-        if b == 0:
-            return a
-        if a == b:
-            return a
-        if a > b:
-            return gcd(a - b, b)
-        return gcd(a, b - a)
+    #     ce=0
+    #     ev=0
+    #     od=0
+    #     ods=1
+    #     for i in range(n):
+    #         ev+=(ce+2)
+    #         ce=ce+2
+    #         od+=(ods+2)
+    #         ods=ods+2
+    #     print(ev,od)
+    #     return self.gcd(ev,od)   
+    # def gcd(self,a,b):
+    #     if a == 0:
+    #         return b
+    #     if b == 0:
+    #         return a
+    #     if a == b:
+    #         return a
+    #     if a > b:
+    #         return gcd(a - b, b)
+    #     return gcd(a, b - a)
+        return n
